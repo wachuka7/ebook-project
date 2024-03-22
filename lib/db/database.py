c9f169cc2e236fe9fb22b8d5282ec2c5d967a1b9
wachuka7 <rachaelwachuka7@gmail.com>
2024-03-23 01:05:03 +0300

    feat: add ORM methods

--- lib/db/database.py
+++ lib/db/database.py
@@ -1,6 +1,5 @@
 from models.book import Book
 from models.author import Author
-
 class Database:
     def __init__(self):
         self.authors = []
@@ -17,4 +16,36 @@ class Database:
         self.books.append(book)
         return book
 
- 
+    def delete_author(self, author):
+        self.authors.remove(author)
+
+    def delete_book(self, book):
+        self.books.remove(book)
+
+    def get_all_authors(self):
+        return self.authors
+
+    def get_all_books(self):
+        return self.books
+
+    def find_author_by_name(self, name):
+        for author in self.authors:
+            if author.name == name:
+                return author
+        return None
+
+    def find_book_by_title(self, title):
+        for book in self.books:
+            if book.title == title:
+                return book
+        return None
+
+    def find_books_by_author(self, author_name):
+        author = self.find_author_by_name(author_name)
+        if author:
+            return author.books
+        else:
+            return []
+
+  
+
